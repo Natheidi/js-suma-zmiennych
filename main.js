751ee00e487278c64accfb6d61aa6bf707b1092d
Natheidi <nathbyrdy@gmail.com>
2023-08-18 14:42:08 +0200

    dodanie kodu

---
+++ main.js
@@ -0,0 +1,11 @@
+let firstNumber = 1;
+let secondNumber = 10;
+
+function dodawanie(number1, number2) {
+    let wynik = number1 + number2;
+    return wynik;
+}
+
+let wynik2 = dodawanie(firstNumber, secondNumber);
+
+console.log(wynik2);
